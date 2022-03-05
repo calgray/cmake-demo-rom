431e83206aea41207c296a8bacd85e267ab26979
Callan Gray <cal.j.gray@gmail.com>
2022-03-06 03:42:47 +0800

    use gcc for linking

--- CMake/Findn64sdkmod.cmake
+++ CMake/Findn64sdkmod.cmake
@@ -1,34 +1,33 @@
 
+
 set(N64SDK_DIR ${CMAKE_SOURCE_DIR}/external/n64sdkmod)
 
-add_library(n64sdkmod::ultra STATIC IMPORTED)
-set(libultra_DIR ${CMAKE_SOURCE_DIR}/external/n64sdkmod)
-set_target_properties(n64sdkmod::ultra
-    PROPERTIES
-    IMPORTED_LOCATION ${libultra_DIR}/lib/n64/libultra.a
-    INTERFACE_INCLUDE_DIRECTORIES ${libultra_DIR}/include/n64
-)
-set(BOOT_SRC ${libultra_DIR}/lib/n64/PR/bootcode/boot.6102)
+set(BOOT_SRC ${N64SDK_DIR}/lib/n64/PR/bootcode/boot.6102)
 set(BOOT_OBJ boot.6102.o)
 
+add_library(n64sdkmod::ultra STATIC IMPORTED)
+set_target_properties(n64sdkmod::ultra
+    PROPERTIES
+    IMPORTED_LOCATION ${N64SDK_DIR}/lib/n64/libultra.a
+    INTERFACE_INCLUDE_DIRECTORIES ${N64SDK_DIR}/include/n64
+)
 add_library(n64sdkmod::nusys STATIC IMPORTED)
-set(nusys_DIR ${CMAKE_SOURCE_DIR}/external/n64sdkmod)
 set_target_properties(n64sdkmod::nusys
     PROPERTIES
-    IMPORTED_LOCATION ${nusys_DIR}/lib/n64/nusys/libnusys.a
-    INTERFACE_INCLUDE_DIRECTORIES ${nusys_DIR}/include/n64/nusys
+    IMPORTED_LOCATION ${N64SDK_DIR}/lib/n64/nusys/libnusys.a
+    INTERFACE_INCLUDE_DIRECTORIES ${N64SDK_DIR}/include/n64/nusys
 )
 
 add_library(n64sdkmod::nualstl STATIC IMPORTED)
 set_target_properties(n64sdkmod::nualstl
     PROPERTIES
-    IMPORTED_LOCATION ${nusys_DIR}/lib/n64/nusys/libnualstl.a
-    INTERFACE_INCLUDE_DIRECTORIES ${nusys_DIR}/include/n64/nusys
+    IMPORTED_LOCATION ${N64SDK_DIR}/lib/n64/nusys/libnualstl.a
+    INTERFACE_INCLUDE_DIRECTORIES ${N64SDK_DIR}/include/n64/nusys
 )
 
 add_library(n64sdkmod::nualsgi STATIC IMPORTED)
 set_target_properties(n64sdkmod::nualsgi
     PROPERTIES
-    IMPORTED_LOCATION ${nusys_DIR}/lib/n64/nusys/libnualsgi.a
-    INTERFACE_INCLUDE_DIRECTORIES ${nusys_DIR}/include/n64/nusys
+    IMPORTED_LOCATION ${N64SDK_DIR}/lib/n64/nusys/libnualsgi.a
+    INTERFACE_INCLUDE_DIRECTORIES ${N64SDK_DIR}/include/n64/nusys
 )
--- CMake/toolchain.n64sdkmod.cmake
+++ CMake/toolchain.n64sdkmod.cmake
@@ -4,12 +4,4 @@ option(TOOLCHAIN_N64SDKMOD "Use N64SDKMOD" ON)
 # TOOLCHAIN_PREFIX - directory containing target gcc compiler and binutils
 # N64SDKMOD_PREFIX - directory containing n64sdkmod
 
-# include_directories(
-#     ${N64SDKMOD_PREFIX}/include
-# )
-
-# link_directories(
-#     ${N64SDKMOD_PREFIX}/lib/n64
-# )
-
 include(${CMAKE_CURRENT_LIST_DIR}/toolchain.mips64-elf.cmake)
